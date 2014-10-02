53bb4370db86d01641783ba0557475203ce10460
emilyadavis303 <emilyadavis303@gmail.com>
2014-10-02 13:49:04 -0600

    Cleans up Resorts index, and adds basic Resort show view.

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -1,5 +1,17 @@
 class ResortsController < ApplicationController
+  before_action :set_resort, only: [:show]
+
   def index
     @resorts = Resort.all
   end
+
+  def show
+
+  end
+
+  private
+
+  def set_resort
+			@resort= Resort.find(params[:id])
+	end
 end
--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,2 +1,5 @@
 class Resort < ActiveRecord::Base
+  def determine_daily_snowfall
+    Snotel.daily(snotel_station.to_sym).last[:change_in_snow_depth_in]
+  end
 end
