de8efecd1e25d112f6ee1af4950b19c3e79c87ab
emilyadavis303 <emilyadavis303@gmail.com>
2014-10-08 14:42:20 -0600

    Displays top three snowfalls on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    @resorts = Resort.all
+    @daily_snowfalls_hash = {}
+
+    @resorts.each {|resort| @daily_snowfalls_hash[resort.name] = resort.determine_daily_snowfall}
+
+    @top_three = Hash[@daily_snowfalls_hash.sort_by{|k,v| v}.reverse].take(3)
   end
 end
