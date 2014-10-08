6a4ffefd99f8ebd6fc29dad7fe7576020f3ee2ed
emilyadavis303 <emilyadavis303@gmail.com>
2014-10-08 14:17:35 -0600

    Sorts by daily snowfall

--- app/controllers/snowfalls_controller.rb
+++ app/controllers/snowfalls_controller.rb
@@ -1,5 +1,10 @@
 class SnowfallsController < ApplicationController
   def index
     @resorts = Resort.all
+    @daily_snowfalls_hash = {}
+
+    @resorts.each {|resort| @daily_snowfalls_hash[resort.name] = resort.determine_daily_snowfall}
+
+    @daily_snowfalls_hash = Hash[@daily_snowfalls_hash.sort_by{|k,v| v}.reverse]
   end
 end
--- app/models/resort.rb
+++ app/models/resort.rb
@@ -1,13 +1,31 @@
 class Resort < ActiveRecord::Base
   def determine_daily_snowfall
-    Snotel.hourly(snotel_station.to_sym).last[:change_in_snow_depth_in]
+    snowfall_daily = Snotel.hourly(snotel_station.to_sym).last[:change_in_snow_depth_in]
+
+    if snowfall_daily.nil?
+      snowfall_daily = 0
+    end
+
+    return snowfall_daily.to_i
   end
 
   def determine_three_day_snowfall
-    Snotel.hourly(snotel_station.to_sym, 72).last[:change_in_snow_depth_in]
+    snowfall_three = Snotel.hourly(snotel_station.to_sym, 72).last[:change_in_snow_depth_in]
+
+    if snowfall_three.nil?
+      snowfall_three = "0"
+    end
+
+    return snowfall_three
   end
 
   def determine_seven_day_snowfall
-    Snotel.daily(snotel_station.to_sym, 7).last[:change_in_snow_depth_in]
+    snowfall_seven = Snotel.daily(snotel_station.to_sym, 7).last[:change_in_snow_depth_in]
+
+    if snowfall_seven.nil?
+      snowfall_seven = "0"
+    end
+
+    return snowfall_seven
   end
 end
