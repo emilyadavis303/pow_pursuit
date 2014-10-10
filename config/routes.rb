1d82c039dee3b31ee4522aab2be8aa2cfc8de08a
emilyadavis303 <emilyadavis303@gmail.com>
2014-10-10 10:06:37 -0600

    OAuth authorizes users or creates users via Twitter.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  match "/auth/twitter/callback" => "sessions#create", via: "get"
+
   resources :resorts
   resources :snowfalls
+  resources :users
 end
