457640c845f76ba2fd2e444a81f4d5b5718010bd
emilyadavis303 <emilyadavis303@gmail.com>
2014-10-15 22:35:00 -0600

    Things.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   match "/auth/twitter/callback" => "sessions#create", via: "get"
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 
   resources :resorts,   only: [:index, :show]
   resources :snowfalls, only: [:index]
