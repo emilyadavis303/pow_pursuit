b79e872eb6590948d562698b40769fb003747237
Emily Davis <emilyadavis303@gmail.com>
2014-11-02 11:15:33 -0700

    organizes css

--- app/assets/javascripts/root_map.js
+++ app/assets/javascripts/root_map.js
@@ -3,14 +3,14 @@ MQA.EventUtil.observe(window, 'load', function() {
   var options = {
     elt: document.getElementById('map'),           // ID of map element on page
     zoom: 9,                                       // initial zoom level of the map
-    latLng: { lat: 39.644801, lng: -106.595289 },  // center of map in latitude/longitude
+    latLng: { lat: 39.638524, lng: -106.170787 },  // center of map in latitude/longitude
     mtype: 'map',                                  // map type (map, sat, hyb); defaults to map
     bestFitMargin: 0,                              // margin offset from map viewport when applying a bestfit on shapes
     zoomOnDoubleClick: true                        // enable map to be zoomed in when double-clicking on map
   };
 
   // construct an instance of MQA.TileMap with the options object
-  window.map = new MQA.TileMapc(options);
+  window.map = new MQA.TileMap(options);
   // download the modules
     MQA.withModule('traffictoggle', 'mousewheel', function() {
 
