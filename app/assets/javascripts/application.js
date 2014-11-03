7b9e1b465b2d41471f5046696bb652fad5a8e258
Emily Davis <emilyadavis303@gmail.com>
2014-11-03 11:20:08 -0700

    trying to fix bootstrap dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-
+//= require jquery
 //= require jquery_ujs
-//= require_tree .
+
 //= require jquery.turbolinks
 //= require bootstrap-sprockets
-//= require jquery
