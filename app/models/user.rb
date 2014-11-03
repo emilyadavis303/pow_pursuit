a10fade57dfc7fdc6e2b480081a580112e99b34e
Emily Davis <emilyadavis303@gmail.com>
2014-11-03 12:55:50 -0700

    adds user avatar default

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :medium => '300x300>', :thumb => '100x100>'}
+  has_attached_file :avatar, :styles => { :medium => '300x300>', :thumb => '100x100>'}, :default_url => '/images/default_avatar.jpg'
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   has_many :user_photos
